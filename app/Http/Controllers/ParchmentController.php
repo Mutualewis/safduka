697c7d56086b8a89071512984ef3239f5001ba37
Gachunga <johnkachunga@gmail.com>
2019-01-24 09:53:16 +0300

    edit

--- app/Http/Controllers/ParchmentController.php
+++ app/Http/Controllers/ParchmentController.php
@@ -119,7 +119,9 @@ class ParchmentController extends Controller {
         $edit->add('st_packages','Packages', 'number')->rule('required|min:1');
         $edit->add('st_bags','Bags', 'number')->rule('required|min:1');
         $edit->add('st_pockets','Pockets', 'number')->rule('required|min:1');
-        
+        $edit->add('cgr_id','Grower','select')->options(coffeegrower::pluck("cgr_grower", "id")->all());
+        $edit->add('pty_id','Partchmant Type','select')->options(ParchmentType::pluck("pty_name", "id")->all());
+        $edit->add('mt_id','Material','select')->options(Material::pluck("mt_name", "id")->all());
         // $edit->add('author_id','Author','select')->options(Author::pluck("firstname", "id")->all());
         // $edit->add('publication_date','Date','date')->format('d/m/Y', 'it');
         // $edit->add('photo','Photo', 'image')->move('uploads/demo/')->fit(240, 160)->preview(120,80);
